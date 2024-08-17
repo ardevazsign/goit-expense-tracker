085ebb382f73dac0a6e5dc3cbf687d1ec1709cf7
owen saavedra <newojunior@gmail.com>
2024-08-17 22:34:45 +0800

    Test page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,16 @@ export const App = () => {
   return (
     <div
       style={{
+        background: 'brown',
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      Expense Tracker
     </div>
   );
 };
